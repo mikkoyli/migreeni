38b2dc72c4291335ccfd34c8417ca578312b80cc
Mikko Yliniemi <mikessu@iki.fi>
2018-04-25 23:03:49 +0300

    diagrammi päivitetty

--- app/src/main/java/harkat/jussi/loppuharkkaproto/MainActivity.java
+++ app/src/main/java/harkat/jussi/loppuharkkaproto/MainActivity.java
@@ -14,17 +14,20 @@ import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    BarGraphSeries<DataPoint> series;
+    GraphView graph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -36,31 +39,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    public void initializeLineGraphView() {
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        //BarGraphSeries<DataPoint> series;
+    private DataPoint[] getData() {
         DatabaseHandler dbhandler = new DatabaseHandler(this);
         final List<Migreeni> list = dbhandler.getMigraines();
         final List<String> _list = new ArrayList<>();
 
-        DataPoint[] dp = new DataPoint[list.size()];
-
+        DataPoint[] dp =new DataPoint[list.size()];
         for (int i = 0;i<list.size() ; i++) {
             _list.add(list.get(i).getDate());
             Log.d("JALAJALA", list.get(i).getDate());
 
-            // todo: tässä pitäisi vissiin jotenki laittaa se migreeni datapointtiin?
-            //dp[i] = new DataPoint(new SimpleDateFormat("yyyy-MM-dd").parse(newStringDate), newDoubleValue);
-        }
+            String dateraw = list.get(i).getDate();
+            Date result = null;
+            DateFormat df = new SimpleDateFormat("dd.MM.YYYY");
+            try {
+                result =  df.parse(dateraw);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                Log.e("JALAJALA","Error with date parsing");
+            }
 
+            // todo: tässä pitäisi vissiin jotenki laittaa se migreeni datapointtiin? new SimpleDateFormat("yyyy-MM-dd").parse(newStringDate)
+            dp[i] = new DataPoint(result, i);
+
+        }
+        return dp;
+    }
+
+    public void initializeLineGraphView() {
+        graph = (GraphView) findViewById(R.id.graph);
         // esimerkki: barchart
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
+        /*BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
                 new DataPoint(3, 2),
                 new DataPoint(4, 6)
-        });
+        });*/
+        series = new BarGraphSeries<DataPoint>(getData());
         graph.addSeries(series);
 
         // styling
